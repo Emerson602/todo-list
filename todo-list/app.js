634502ad4b382fa92df3d7769e68a862f7bd969d
emerson602 <emerson.nic@hotmail.com>
2023-12-13 10:32:19 -0300

    update

--- todo-list/app.js
+++ todo-list/app.js
@@ -12,7 +12,7 @@ const app = {
         getCurrentDateTime() {
            setInterval(() => {
                  const currentDateTime = new Date();
-                 const options = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric', hour: 'numeric', minute: 'numeric', second: 'numeric'};
+                 const options = { year: 'numeric', month: 'long', day: 'numeric', hour: 'numeric', minute: 'numeric'};
                  const formattedDateTime = currentDateTime.toLocaleDateString('pt-BR', options);
                  localStorage.setItem("DateTime", formattedDateTime);
                  this.dataTime = localStorage.getItem("DateTime")
@@ -26,10 +26,10 @@ const app = {
                 count += 1;
                 const taskDescription = localStorage.getItem(`task-${count}`);
                 const createIn = localStorage.getItem(`create-in-${count}`)
-                this.tasksList.push(`Criado em: (${createIn}) <br><br> ${taskDescription}`); 
-            }               
+                
+                this.tasksList.push(`<span id="date-time" class="bg-light">${createIn}</span><span class="bg-light  mt-3">${taskDescription}</span>`); 
+            }     
                
-            
         },
 
         addTasks() {
@@ -42,11 +42,12 @@ const app = {
                 this.taskInput = '';
 
                 this.getTasks();
+            } else {
+                alert('O campo está vazio! Digite uma tarefa')
             }
         },
 
-        removeTask(index) {  
-
+        removeTask(index) { 
             const taskId = index + 1; 
             localStorage.removeItem(`task-${taskId}`);
             
